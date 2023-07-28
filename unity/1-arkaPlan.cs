1c9faccecb4d67232cfb264758a2edb2d10adfbf
Salim Aydın <99368865+salim-aydin@users.noreply.github.com>
2023-07-28 20:04:56 +0300

    Update 1-arkaPlan.cs

--- unity/1-arkaPlan.cs
+++ unity/1-arkaPlan.cs
@@ -14,3 +14,16 @@ Bu component'ler, GameObject'in özelliklerini ve davranışlarını tanımlar.
 Örneğin, bir GameObject'in bir Transform bileşeni, bir Sprite Renderer bileşeni, bir Collider bileşeni ve bir RigidBody2D bileşeni olabilir.
 
   
+Dolayısıyla, GetComponent<Rigidbody2D>() ifadesi, GameObject'deki bileşen listesini kontrol eder, ilk RigidBody2D bileşenini bulur ve onu döndürür.
+GetComponent metodu, genellikle bir GameObject'in dinamik olarak davranmasına veya başka bir bileşenle etkileşime girmesine izin vermek için kullanılır
+GameObject'in RigidBody2D'sini alabilir ve ardından bu RigidBody2D'nin fiziksel özelliklerini (örneğin hızı veya yönü) değiştirebilirsiniz.
+
+Bu kod, bir Unity GameObject'den belirli bir türde bir bileşen (component) almak için kullanılan bir metodun pseudo-kodudur. 
+Unity'de, her GameObject'in bir dizi bileşeni vardır.     
+Bu bileşenler, GameObject'in özelliklerini ve davranışlarını tanımlar
+GameObject'in bir Transform bileşeni, bir Sprite Renderer bileşeni, bir Collider bileşeni ve bir RigidBody2D bileşeni olabilir.
+Şimdi kodun her satırını ayrıntılı bir şekilde açıklayalım:
+public ComponentType GetComponent<ComponentType>()
+Bu satır, GetComponent adında bir fonksiyonun tanımını başlatır. 
+Bu fonksiyon, herhangi bir giriş parametresi almayan ve ComponentType türünde bir değer döndüren bir fonksiyondur.
+ComponentType, bir yer tutucu veya jenerik parametre olup, bu fonksiyonu çağırdığınızda hangi türde bir bileşen istediğinizi belirtir.
